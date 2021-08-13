da1f2fb871b738bfe2e38b18cab823cf9bea2df3
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-14 00:44:54 +0530

    increase done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, inCount] = useState(0);
+  // const [count, deCount] = useState(10);
+  function increase() {
+    inCount(count + 1);
+  }
+  // function decrese() {
+  //   deCount(count - 1);
+  // }
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      {/* <button onClick={decrese}>-</button> */}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-let count = 0;
-
-function increase() {
-  console.log("got cliked");
-}
-ReactDOM.render(
-  <div className="container">
-    <h1>{count}</h1>
-    <button onClick={increase}>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
